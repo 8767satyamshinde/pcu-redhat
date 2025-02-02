d5d20b93203849bc1aa869db997ab0d2f79a6d48
Satyam Shinde <shindesatyam72@gmail.com>
2025-02-02 17:25:39 +0530

    weather

--- weather/app.js
+++ weather/app.js
@@ -1,12 +1,15 @@
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
+
+
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 
 var app = express();
+var path = require('path');
+var cookieParser = require('cookie-parser');
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
