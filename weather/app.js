ed6b987a86ec9692e2705b538a9252d9469fe6b2
Satyam Shinde <shindesatyam72@gmail.com>
2025-02-02 17:28:52 +0530

    weather

--- weather/app.js
+++ weather/app.js
@@ -1,17 +1,35 @@
 var createError = require('http-errors');
 var express = require('express');
-
-
 var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var path = require('path');
+var cors = require('cors');
+var helmet = require('helmet');
+var compression = require('compression');
+var rateLimit = require('express-rate-limit');
 
 var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users'); // Example additional route
 
 var app = express();
-var path = require('path');
-var cookieParser = require('cookie-parser');
 
+// Security middleware
+app.use(helmet());
 
-// view engine setup
+// Enable CORS
+app.use(cors());
+
+// Enable compression
+app.use(compression());
+
+// Rate limiter to prevent abuse
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+});
+app.use(limiter);
+
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -21,20 +39,26 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routes
 app.use('/', indexRouter);
+app.use('/users', usersRouter); // Additional API route
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+// Custom middleware example
+app.use((req, res, next) => {
+  console.log(`Request received: ${req.method} ${req.url}`);
+  next();
+});
+
+// Catch 404 and forward to error handler
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+// Error handler
+app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
